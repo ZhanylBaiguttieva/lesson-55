f4c2c879acff579b902dce8ecff927756d04a43f
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-04 02:16:58 +0600

    Created types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+export interface Ingredient {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+}
+
+export interface BurgerIngredient {
+  id: number;
+  name: string;
+  count: number;
+}
